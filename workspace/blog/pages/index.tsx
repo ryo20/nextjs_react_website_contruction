52c10e41f3351e392a8314bb2500b56162b11d6a
ryo20 <54426977+ryo20@users.noreply.github.com>
2022-11-21 18:47:45 +0900

    feat: recent posts in home

--- workspace/blog/pages/index.tsx
+++ workspace/blog/pages/index.tsx
@@ -1,8 +1,15 @@
 import Container from "components/container"
 import Hero from "components/hero"
 import Meta from "components/meta"
+import Posts from "components/posts"
+import Pagination from "components/pagination"
+import { getAllPosts } from "lib/api"
+import { getPlaiceholder } from "plaiceholder"
+import { eyecatchLocal } from "lib/constants"
 
-export default function Home() {
+export default function Home({ posts }: {
+  posts: { title: string, slug: string, eyecatch: { url: string, height: number, width: number } }[]
+}) {
   return (
     <Container>
       <Meta />
@@ -11,6 +18,22 @@ export default function Home() {
         subtitle="会うトップっとしていくサイト"
         imageOn={true}
       />
+      <Posts posts={posts} />
+      <Pagination nextUrl="/blog" nextText="More Posts" />
     </Container>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await getAllPosts(4)
+  for (const post of posts) {
+    if (!Object.hasOwn(post, "eyecatch")) {
+      post.eyecatch = eyecatchLocal
+    }
+  }
+  return {
+    props: {
+      posts: posts,
+    }
+  }
 }
