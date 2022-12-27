e57cf7fb9e0d17d6dd596af1ea8197d78832441d
ryo20 <54426977+ryo20@users.noreply.github.com>
2022-12-27 09:57:15 +0900

    feat: gtag

--- workspace/blog/pages/_app.tsx
+++ workspace/blog/pages/_app.tsx
@@ -1,6 +1,10 @@
 import 'styles/output.css'
 import Layout from 'components/layout'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import * as gtag from 'lib/gtag'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 // Font Awesomeの設定
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -8,9 +12,38 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {/* グローバルサイトタグのインストール */}
+      <Script
+        strategy='afterInteractive'
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
