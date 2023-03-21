eff3c294e5cf314513dab4f916c639c653948f2c
ryo20 <54426977+ryo20@users.noreply.github.com>
2023-03-21 11:05:41 +0900

    fix: type

--- workspace/blog/lib/mongodb.ts
+++ workspace/blog/lib/mongodb.ts
@@ -2,7 +2,7 @@
 
 import { MongoClient } from 'mongodb'
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI || ""
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
