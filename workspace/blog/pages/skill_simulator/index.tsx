ec898a9ff4af070ea50197b234dd8ff6ea05fb66
ryo20 <54426977+ryo20@users.noreply.github.com>
2023-03-31 06:31:50 +0900

    feat: grid

--- workspace/blog/pages/skill_simulator/index.tsx
+++ workspace/blog/pages/skill_simulator/index.tsx
@@ -1,27 +1,49 @@
 // 現状はmongodb/index.tsxのコピー
 import clientPromise from "lib/mongodb";
 import { Armor } from "lib/mongodb";
+import Container from "components/container"
+import Hero from "components/hero"
 
 
 export default function Home({ armors }: { armors: Armor[] }) {
   return (
-    <div className="container">
-      <div>
-        {armors.map((armor, index) => {
-          return (
-            <div className="card" key={index}>
-              <h2>{armor.name}</h2>
-              <p>{armor.rarity}</p>
-              <p>{armor.equipment_type}</p>
-              <p>{armor.defense}</p>
-              <p>{armor.resistance.fire}{armor.resistance.water}{armor.resistance.thunder}{armor.resistance.ice}{armor.resistance.dragon}</p>
-              <p>{JSON.stringify(armor.skills)}</p>
-              <p>{armor.slots}</p>
-            </div>
-          );
-        })}
+    <Container>
+      <Hero
+        title="MHRise Sunbreak スキルシミュレータ"
+        subtitle="頑張る"
+        imageOn={true}
+      />
+      <div className="grid grid-cols-5 gap-2 mt-2 mb-8 text-xl">
+        <div className="form-control w-full max-w-x border-2 border-gray-200">
+          <label className="label">
+            <span className="label-text">Attack Boost</span>
+          </label>
+          <br />
+          <select className="select select-primary w-full">
+            <option disabled selected>lv0</option>
+            <option>Star Wars</option>
+            <option>Harry Potter</option>
+            <option>Lord of the Rings</option>
+            <option>Planet of the Apes</option>
+            <option>Star Trek</option>
+          </select>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Attack Boost</span>
+          </label>
+          <br />
+          <select className="select  select-lg select-bordered">
+            <option disabled selected>lv0</option>
+            <option>Star Wars</option>
+            <option>Harry Potter</option>
+            <option>Lord of the Rings</option>
+            <option>Planet of the Apes</option>
+            <option>Star Trek</option>
+          </select>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
