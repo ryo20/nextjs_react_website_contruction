de2e7ad332c497ffd016bed506f04e2dfc83cc6d
ryo20 <54426977+ryo20@users.noreply.github.com>
2023-04-13 07:34:23 +0900

    feat: wip calculate_armor_score

--- workspace/blog/pages/api/form.ts
+++ workspace/blog/pages/api/form.ts
@@ -12,6 +12,7 @@ type Data = {
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   // 検索対象スキルの確認
   const body = JSON.parse(JSON.stringify(req.body))
+  console.log(body)
   const query = Object.getOwnPropertyNames(body).map((name) => { return { "skills.name": `${name}` } })
   console.log(query)
   // db検索
@@ -19,9 +20,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const db = client.db("mhrsb");
   let armors = await db.collection("sample_armors").find({
     $or: query
-  }).limit(10).toArray()
+  }).limit(10).toArray() as Armor[]
 
   // TODO:装備組み合わせの検索
+  explore_armor_set(armors, body)
   armors = JSON.parse(JSON.stringify(armors))
   // console.log(armors)
   res.json({ data: JSON.stringify(armors) })
@@ -29,9 +31,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
 // FIXME:any型の置き換え
 /** 装備の組み合わせを探索する */
-function explore_armor_set(armors: Armor[], body: any) {
-  // 対象スキルの名前とそのレベルを取得
-  // 対象スキルの重みを取得（skill.slot）
+async function explore_armor_set(armors: Armor[], body: any) {
+  const query = Object.getOwnPropertyNames(body).map((name) => { return { "name": `${name}` } })
+  console.log(query)
+  // db検索より対象スキルの重みを取得（skill.slot）
+  const client = await clientPromise;
+  const db = client.db("mhrsb");
+  let skills = await db.collection("skills").find({
+    $or: query
+  }).toArray() as Skill[]
+  skills.map((skill) => skill.request_level = Number(body[skill.name]))
+  armors.map((armor) => armor.score = calculate_armor_score(armor, skills))
+  // console.log(skills)
+  console.log(armors)
   // 装備にスコアリング(score = sum(skill.slot * armor.skill.level) + defense/1000)
-  // 各部位
+
+  // 各部位のスコア上位から組み合わせを探索する
+  // 探索回数上限に達したら、要求を達成できているか>スコア総和降順という２段階のソートの上、組み合わせを返却する
+}
+
+/**
+ *
+ * @param armor スコア付与対象の装備
+ * @param skills 要求スキルセット
+ * @returns armorに対するskillsに基づくスコア
+ */
+function calculate_armor_score(armor: Armor, skills: Skill[]): number {
+  let score = 0
+  return score
 }
