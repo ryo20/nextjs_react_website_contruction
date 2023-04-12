498322c50135705b22af36152edd15e99d02435a
ryo20 <54426977+ryo20@users.noreply.github.com>
2023-04-13 06:36:00 +0900

    feat: fetch match armors

--- workspace/blog/pages/api/form.ts
+++ workspace/blog/pages/api/form.ts
@@ -1,26 +1,37 @@
+// ref:フォーム作成 https://nextjs.org/docs/guides/building-forms
+// ref:検索クエリ https://fits.hatenablog.com/entry/2018/12/10/004155
+// https://www.mongodb.com/docs/manual/reference/operator/query/or/
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import clientPromise, { Armor, Skill } from "lib/mongodb";
 
 type Data = {
   data: string
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  // Get data submitted in request's body.
-  const body = req.body
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  // 検索対象スキルの確認
+  const body = JSON.parse(JSON.stringify(req.body))
+  const query = Object.getOwnPropertyNames(body).map((name) => { return { "skills.name": `${name}` } })
+  console.log(query)
+  // db検索
+  const client = await clientPromise;
+  const db = client.db("mhrsb");
+  let armors = await db.collection("sample_armors").find({
+    $or: query
+  }).limit(10).toArray()
 
-  // Optional logging to see the responses
-  // in the command line where next.js app is running.
-  console.log('body: ', body)
+  // TODO:装備組み合わせの検索
+  armors = JSON.parse(JSON.stringify(armors))
+  // console.log(armors)
+  res.json({ data: JSON.stringify(armors) })
+}
 
-  // Guard clause checks for first and last name,
-  // and returns early if they are not found
-  // if (!body.first || !body.last) {
-  //   // Sends a HTTP bad request error code
-  //   return res.status(400).json({ data: 'First or last name not found' })
-  // }
-
-  // Found the name.
-  // res.json({ data: `回避${body.kaihi}\n攻撃${body.kougeki}\n防御${body.bougyo}` })
-  res.json({ data: JSON.stringify(body) })
+// FIXME:any型の置き換え
+/** 装備の組み合わせを探索する */
+function explore_armor_set(armors: Armor[], body: any) {
+  // 対象スキルの名前とそのレベルを取得
+  // 対象スキルの重みを取得（skill.slot）
+  // 装備にスコアリング(score = sum(skill.slot * armor.skill.level) + defense/1000)
+  // 各部位
 }
